d5cafc2b2a1b7a397292a6e8b0cb1032ef29ffc8
Nick <nconkl1@outlook.com>
2023-05-08 13:25:05 -0400

    created upload page

---
+++ src/routes/upload/+page.server.ts
@@ -0,0 +1,1 @@
+// Add to this in future branch
---
+++ src/routes/upload/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { FileDropzone } from '@skeletonlabs/skeleton';
+</script>
+
+<div class="container mx-auto justify-center items-center">
+	<form method="POST">
+		<FileDropzone name="files" enctype="multipart/form-data" accept=".har" />
+	</form>
+</div>
