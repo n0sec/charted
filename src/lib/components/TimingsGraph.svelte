bb8de3561093734a294ae1b51a27cd218aef8ebc
Nick <nconkl1@outlook.com>
2023-05-10 19:57:32 -0400

    created timing graph

--- src/lib/components/TimingsGraph.svelte
+++ src/lib/components/TimingsGraph.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
-	import type { HarEntry } from '$lib/types/HarEntry';
-
-	export let entries: HarEntry[] = [];
-
-	import * as echarts from 'echarts/core';
-	import { GridComponent } from 'echarts/components';
+	import type { HarEntry, Timings } from '$lib/types/HarEntry';
 	import { BarChart } from 'echarts/charts';
+	import { GridComponent } from 'echarts/components';
+	import * as echarts from 'echarts/core';
 	import { CanvasRenderer } from 'echarts/renderers';
 	import { onMount } from 'svelte';
 
+	export let entries: HarEntry[] = [];
+
 	echarts.use([GridComponent, BarChart, CanvasRenderer]);
 	let chartContainer: HTMLDivElement;
-	let height = '600px';
+	let height = '400px';
 
 	const option = {
 		xAxis: {
@@ -19,11 +18,26 @@
 			data: ['Blocked', 'Connect', 'DNS', 'Receive', 'Send', 'SSL', 'Wait']
 		},
 		yAxis: {
-			type: 'value'
+			axisLabel: {
+				formatter: '{value} ms',
+				align: 'right'
+			},
+			min: 0,
+			type: 'value',
+			name: 'Response Time (in ms)'
+		},
+		label: {
+			show: true,
+			position: 'top',
+			// ! FIXME: This shows up weird. Need to fix
+			// Negative values shouldn't be added together, they should just be 0
+			formatter: function (params) {
+				return Math.round((Number(params.value) + Number.EPSILON) * 100) / 100 + ' ms';
+			}
 		},
 		series: [
 			{
-				data: entries.timings,
+				data: getTimings(),
 				type: 'bar'
 			}
 		]
@@ -33,6 +47,38 @@
 		const chart = echarts.init(chartContainer);
 		chart.setOption(option);
 	});
+
+	function getTimings() {
+		const timings = entries
+			.flatMap((o) => o.timings!)
+			.reduce(
+				(acc, obj) => {
+					return {
+						blocked: acc.blocked! + (obj.blocked ?? 0),
+						connect: acc.connect! + (obj.connect ?? 0),
+						dns: acc.dns! + (obj.dns ?? 0),
+						receive: acc.receive! + (obj.receive ?? 0),
+						send: acc.send! + (obj.send ?? 0),
+						ssl: acc.ssl! + (obj.ssl ?? 0),
+						wait: acc.wait! + (obj.wait ?? 0)
+					} as Timings;
+				},
+				{
+					//needs to be in the same order from the xAxis
+					blocked: 0,
+					connect: 0,
+					dns: 0,
+					receive: 0,
+					send: 0,
+					ssl: 0,
+					wait: 0
+				}
+			);
+		return Object.values(timings);
+	}
 </script>
 
-<div bind:this={chartContainer} style="width: 100%;" style:height />
+<div class="col-span-2">
+	<h1 class="unstyled text-xl font-bold mb-1">Timing Totals by Type</h1>
+	<div bind:this={chartContainer} style="width: 100%;" style:height />
+</div>
