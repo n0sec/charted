0b2c3beeadd1902b73306f9aba78fd4004c770d3
Nick <nconkl1@outlook.com>
2023-05-11 19:01:09 -0400

    added components to page layout

--- src/lib/components/HARResults.svelte
+++ src/lib/components/HARResults.svelte
@@ -1,14 +1,28 @@
 <script lang="ts">
 	import type { HarEntry } from '$lib/types/HarEntry';
+	import FourHundredCodeDetails from './FourHundredCodeDetails.svelte';
+	import FiveHundredCodeDetails from './FiveHundredCodeDetails.svelte';
 	import RequestedUrlTable from './RequestedURLTable.svelte';
+	import ResponseCodes from './ResponseCodes.svelte';
+	import ResponseCodesPieChart from './ResponseCodesPieChart.svelte';
 	import TimingsGraph from './TimingsGraph.svelte';
 	import TimingsTable from './TimingsTable.svelte';
+	import PageLoadDetails from './PageLoadDetails.svelte';
 	export let entries: HarEntry[] = [];
 </script>
 
 <div class="mx-6 mt-6 space-y-5">
+	<PageLoadDetails {entries} />
+	<ResponseCodes {entries} />
+	<ResponseCodesPieChart {entries} />
+	{#if entries.filter((code) => code.response?.status >= 400 && code.response?.status < 500).length > 0}
+		<FourHundredCodeDetails {entries} />
+	{/if}
+	{#if entries.filter((code) => code.response?.status >= 500 && code.response?.status < 600).length > 0}
+		<FiveHundredCodeDetails {entries} />
+	{/if}
 	<RequestedUrlTable {entries} />
-	<div class="grid grid-cols-5 gap-x-3">
+	<div class="grid grid-cols-5 gap-x-10">
 		<TimingsTable {entries} />
 		<TimingsGraph {entries} />
 	</div>
