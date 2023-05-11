d7b8604faf95d29defb12f9519adf4eec44e1f93
Nick <nconkl1@outlook.com>
2023-05-11 11:20:14 -0400

    adding svelte table component - WIP

--- src/lib/components/RequestedURLTable.svelte
+++ src/lib/components/RequestedURLTable.svelte
@@ -1,12 +1,39 @@
 <script lang="ts">
 	import type { HarEntry } from '$lib/types/HarEntry';
+	import { Table, tableMapperValues, type TableSource } from '@skeletonlabs/skeleton';
 	export let entries: HarEntry[] = [];
+
+	const sourceData = [
+		entries.map((entry) => entry.request?.url),
+		entries.map((entry) => entry.request?.method),
+		entries.map((entry) => entry.response?.status),
+		entries.map((entry) =>
+			entry.request?.method == 'GET'
+				? Math.round((Number(entry.response?.bodySize) + Number.EPSILON) * 100) / 100
+				: Math.round((Number(entry.request?.bodySize) + Number.EPSILON) * 100) / 100
+		),
+		entries.map((entry) => Math.round((Number(entry.time) + Number.EPSILON) * 100) / 100),
+		entries.map((entry) => entry.serverIPAddress)
+	];
+
+	const requestTable: TableSource = {
+		head: ['URL', 'Method', 'Status', 'Size', 'Time', 'Server IP Address'],
+		body: tableMapperValues(sourceData, [
+			'url',
+			'method',
+			'status',
+			'size',
+			'time',
+			'serverIPAddress'
+		]),
+		foot: []
+	};
 </script>
 
-<!-- Responsive Container (recommended) -->
-<div class="table-container rounded-none">
+<Table source={requestTable} />
+
+<!-- <div class="table-container rounded-none">
 	<h1 class="unstyled text-xl font-bold mb-1">Request Data with Response Times</h1>
-	<!-- Native Table Element -->
 	<table class="table table-hover table-compact rounded-none">
 		<thead class="text-sm">
 			<tr>
@@ -35,8 +62,6 @@
 				</tr>
 			{/each}
 		</tbody>
-		<tfoot>
-			<!-- ? Add Pagination here? -->
-		</tfoot>
+		<tfoot />
 	</table>
-</div>
+</div> -->
