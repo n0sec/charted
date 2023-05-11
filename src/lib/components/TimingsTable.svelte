9607ea620b627deebdedce7493a57e12b6ad5acf
Nick <nconkl1@outlook.com>
2023-05-11 19:00:07 -0400

    replaced Tailwind table for Skeleton Svelte Table

--- src/lib/components/TimingsTable.svelte
+++ src/lib/components/TimingsTable.svelte
@@ -1,44 +1,52 @@
 <script lang="ts">
 	import type { HarEntry } from '$lib/types/HarEntry';
+	import { Paginator, Table, tableMapperValues, type TableSource } from '@skeletonlabs/skeleton';
+	import type { PaginationSettings } from '@skeletonlabs/skeleton/dist/components/Paginator/types';
 	export let entries: HarEntry[] = [];
+
+	const sourceData = entries.map((entry) => {
+		return {
+			url: entry.request?.url,
+			blocked: Math.round((Number(entry.timings?.blocked) + Number.EPSILON) * 100) / 100,
+			connect: Math.round((Number(entry.timings?.connect) + Number.EPSILON) * 100) / 100,
+			dns: Math.round((Number(entry.timings?.dns) + Number.EPSILON) * 100) / 100,
+			receive: Math.round((Number(entry.timings?.receive) + Number.EPSILON) * 100) / 100,
+			send: Math.round((Number(entry.timings?.send) + Number.EPSILON) * 100) / 100,
+			ssl: Math.round((Number(entry.timings?.ssl) + Number.EPSILON) * 100) / 100,
+			wait: Math.round((Number(entry.timings?.wait) + Number.EPSILON) * 100) / 100
+		};
+	});
+
+	const timingsTable: TableSource = {
+		head: ['URL', 'Blocked', 'Connect', 'DNS', 'Receive', 'Send', 'SSL', 'Wait'],
+		body: tableMapperValues(sourceData, [
+			'url',
+			'blocked',
+			'connect',
+			'dns',
+			'receive',
+			'send',
+			'ssl',
+			'wait'
+		]),
+		foot: []
+	};
+
+	// Paginator Settings
+	let page: PaginationSettings = {
+		offset: 0,
+		limit: 10,
+		size: sourceData.length,
+		amounts: [1, 2, 5, 10]
+	};
 </script>
 
-<!-- Responsive Container (recommended) -->
-<div class="table-container rounded-none col-span-3">
-	<h1 class="unstyled text-xl font-bold mb-1">Request Timings</h1>
-
-	<!-- Native Table Element -->
-	<table class="table table-hover table-compact rounded-none">
-		<thead class="text-sm">
-			<tr>
-				<th>URL</th>
-				<th>Blocked</th>
-				<th>Connect</th>
-				<th>DNS</th>
-				<th>Receive</th>
-				<th>Send</th>
-				<th>SSL</th>
-				<th>Wait</th>
-			</tr>
-		</thead>
-		<tbody>
-			{#each entries as entry}
-				<tr>
-					<td>{entry.request?.url}</td>
-					<td>{Math.round((Number(entry.timings?.blocked) + Number.EPSILON) * 100) / 100}</td>
-					<td>{Math.round((Number(entry.timings?.connect) + Number.EPSILON) * 100) / 100}</td>
-					<td>{Math.round((Number(entry.timings?.dns) + Number.EPSILON) * 100) / 100}</td>
-					<td>{Math.round((Number(entry.timings?.receive) + Number.EPSILON) * 100) / 100}</td>
-					<td>{Math.round((Number(entry.timings?.send) + Number.EPSILON) * 100) / 100}</td>
-					<td>{Math.round((Number(entry.timings?.ssl) + Number.EPSILON) * 100) / 100}</td>
-					<td>{Math.round((Number(entry.timings?.wait) + Number.EPSILON) * 100) / 100}</td>
-				</tr>
-			{/each}
-		</tbody>
-		<tfoot>
-			<tr>
-				<!-- ? May add this later-->
-			</tr>
-		</tfoot>
-	</table>
+<div class="rounded-none col-span-3 text-sm table-compact">
+	<h1 class="unstyled text-lg ml-3 font-bold mb-1">Request Timings</h1>
+	<Table source={timingsTable} />
+	<div class="mt-3">
+		{#if sourceData.length > 10}
+			<Paginator bind:settings={page} />
+		{/if}
+	</div>
 </div>
