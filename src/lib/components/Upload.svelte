a6d7b624f80b4b491006344e319bb96ba86ef6d0
Nick <nconkl1@outlook.com>
2023-05-12 19:00:45 -0400

    misc wording changes

--- src/lib/components/Upload.svelte
+++ src/lib/components/Upload.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { browser } from '$app/environment';
 	import { enhance } from '$app/forms';
-	import type { HarEntry } from '$lib/types/HarEntry';
 	import { FileDropzone } from '@skeletonlabs/skeleton';
 	import { createEventDispatcher } from 'svelte';
 
@@ -19,28 +18,6 @@
 
 				const harEntries = harData.log.entries;
 
-				// If harEntries is > 10 entries, split into equal parts
-				// and dispatch each part to the HarResults component
-				// to be rendered
-				// if (harEntries.length > 10) {
-				// 	const harEntriesSplit = harEntries.reduce((resultArray: HarEntry[][], item, index) => {
-				// 		const chunkIndex = Math.floor(index / 10);
-
-				// 		if (!resultArray[chunkIndex]) {
-				// 			resultArray[chunkIndex] = []; // start a new chunk
-				// 		}
-
-				// 		resultArray[chunkIndex].push(item);
-
-				// 		return resultArray;
-				// 	}, []);
-
-				// 	harEntriesSplit.forEach((harEntries: HarEntry[]) => {
-				// 		dispatch('change', { error: false, entries: harEntries });
-				// 	});
-				// } else {
-				// 	dispatch('change', { error: false, entries: harEntries });
-				// }
 				dispatch('change', { error: false, entries: harEntries });
 			};
 			reader.readAsText(file);
@@ -58,7 +35,7 @@
 				<iconify-icon icon="ic:outline-file-upload" width="32" height="32" class="mb-0" />
 			</svelte:fragment>
 			<svelte:fragment slot="message"><b>Upload a file</b> or drag and drop</svelte:fragment>
-			<svelte:fragment slot="meta">HAR files only - 10 MB size limit</svelte:fragment>
+			<svelte:fragment slot="meta">HAR files only</svelte:fragment>
 		</FileDropzone>
 		{#if !browser}
 			<button
