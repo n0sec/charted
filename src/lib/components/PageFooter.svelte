ea57d928627aa231033fc805fecff0d3a5fd1c16
Nick <nconkl1@outlook.com>
2023-05-15 12:52:27 -0400

    added page footer

---
+++ src/lib/components/PageFooter.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import 'iconify-icon';
+</script>
+
+<div class="page-footer border-t border-surface-500/50">
+	<div
+		class="container mx-auto grid grid-cols-[1fr_auto] grid-rows-5 align-items-middle gap-y-1 py-4"
+	>
+		<strong>&copy; 2023 cHARted</strong>
+		<div class="row-start-2">HAR Analyzer & Network Metrics</div>
+		<a
+			href="https://github.com/n0sec/charted/issues/new"
+			target="_blank"
+			class="no-underline row-start-3">Submit a bug</a
+		>
+		<div class="col-start-2">
+			Made With <iconify-icon
+				icon="mdi:cards-heart-outline"
+				style="color: red;"
+				class="align-middle text-lg"
+			/> Using
+		</div>
+		<a href="https://skeleton.dev">Skeleton UI</a>
+		<a href="https://kit.svelte.dev">SvelteKit</a>
+		<a href="https://svelte.dev" class="row-start-4 col-start-2">Svelte</a>
+		<a href="https://tailwindcss.com" class="row-start-5 col-start-2">TailwindCSS</a>
+	</div>
+</div>
